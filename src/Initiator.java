093c0b8061175be6998dc20ce2cf1ae2e44e0598
manoj <manojg@wwso2.com>
2016-04-15 22:05:07 +0530

    config changes

--- src/Initiator.java
+++ src/Initiator.java
@@ -27,7 +27,7 @@ public class Initiator {
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("Content-Type", "application/json");
 			
-			String input = readFile("/home/manoj/support/verizone/workspace/MesosClusterSetup/conf/apim/mgtgw2.json",Charset.defaultCharset());
+			String input = readFile("/home/manoj/support/verizone/workspace/MesosClusterSetup/conf/apim/gw1.json",Charset.defaultCharset());
 			System.out.println(input);
 
 			OutputStream os = conn.getOutputStream();
